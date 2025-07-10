76c7eb6b85be41e03bd8e57ecc5e8a50a9709fee
bevanwsjones <bevanwsjones@gmail.com>
2025-07-10 12:27:22 +0200

    - added spacing enumeration to layout. - paragraphs will now be justified.

--- src/components/atoms/layout.rs
+++ src/components/atoms/layout.rs
@@ -1,10 +1,28 @@
 
 use leptos::prelude::*;
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Spacing{
+    Sm, // 8 (default)
+    Md, // 16
+    Lg, // 24
+}
+
+fn space_to_string(space: Spacing) -> &'static str{
+    return match space {
+        Spacing::Sm => "2",
+        Spacing::Md => "3",
+        Spacing::Lg => "4"
+    };
+}
+
 #[component]
-pub fn Stack(children: Children) -> impl IntoView {
+pub fn Stack(
+    #[prop(default = Spacing::Sm)] space: Spacing,
+    children: Children
+) -> impl IntoView {
     
-    let class_str:String = "space-y-2".to_string();
+    let class_str:String = format!("space-y-{}", space_to_string(space)).to_string();
     return view!{
         <div class={class_str}>
             {children()}
--- src/components/atoms/typography.rs
+++ src/components/atoms/typography.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn H1(children: Children) -> impl IntoView {
     return view! {
-        <h1 class="text-2xl font-primary font-bold text-content-primary dark:text-content-primary-dark">
+        <h1 class="font-primary font-bold text-2xl text-content-primary dark:text-content-primary-dark">
             {children()}
         </h1>
     };
@@ -12,7 +12,7 @@ pub fn H1(children: Children) -> impl IntoView {
 #[component]
 pub fn H2(children: Children) -> impl IntoView {
     return view! {
-        <h2 class="text-xl font-primary font-semibold text-content-primary dark:text-content-primary-dark">
+        <h2 class="font-primary font-semibold text-xl text-content-primary dark:text-content-primary-dark">
             {children()}
         </h2>
     };
@@ -21,7 +21,7 @@ pub fn H2(children: Children) -> impl IntoView {
 #[component]
 pub fn H3(children: Children) -> impl IntoView {
     return view! {
-        <h3 class="text-lg font-primary font-medium text-content-primary dark:text-content-primary-dark">
+        <h3 class="font-primary font-medium text-lg text-content-primary dark:text-content-primary-dark">
             {children()}
         </h3>
     };
@@ -30,7 +30,7 @@ pub fn H3(children: Children) -> impl IntoView {
 #[component]
 pub fn H4(children: Children) -> impl IntoView {
     return view! {
-        <h4 class="text-base font-primary font-medium text-content-primary dark:text-content-primary-dark">
+        <h4 class="font-primary font-medium text-base text-content-primary dark:text-content-primary-dark">
             {children()}
         </h4>
     };
@@ -39,7 +39,7 @@ pub fn H4(children: Children) -> impl IntoView {
 #[component]
 pub fn P(children: Children) -> impl IntoView {
     return view! {
-        <h4 class="text-base font-primary font-normal text-content-primary dark:text-content-primary-dark">
+        <h4 class="font-primary font-normal text-base text-justify text-content-primary dark:text-content-primary-dark">
             {children()}
         </h4>
     };
