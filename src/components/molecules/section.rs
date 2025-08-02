d41ac3f3e733558f7d812844269d5846dab14115
bevanwsjones <bevanwsjones@gmail.com>
2025-08-02 18:03:27 +0200

    - added additional layout options fro sections.

--- src/components/molecules/section.rs
+++ src/components/molecules/section.rs
@@ -1,7 +1,7 @@
 use leptos::prelude::*;
 
 use crate::common::size::*;
-use crate::components::atoms::layout::{spacing, Stack};
+use crate::components::atoms::layout::{spacing, Align, FlexAlign, Stack};
 use crate::components::atoms::typography::{H1, H2, H3, H4};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -15,11 +15,13 @@ pub enum SectionLevel {
 #[component]
 pub fn Section(
     #[prop(default = false)] is_first: bool,
+    #[prop(default = false)] centre: bool,
+    #[prop(default = false)] spaced: bool,
     level: SectionLevel,
     title: String,
     children: Children,
 ) -> impl IntoView {
-    let space_above = if !is_first {
+    let space = if !is_first {
         match level {
             SectionLevel::H1 => spacing(Size::Xl),
             SectionLevel::H2 => spacing(Size::Lg),
@@ -30,19 +32,22 @@ pub fn Section(
         spacing(Size::None)
     };
 
+    let class_str = format!("mt-{} mb-{}", space, space);
+    let align = if centre { Align::Center } else { Align::Left };
+
     let heading = match level {
-        SectionLevel::H1 => view! { <H1>{title.clone()}</H1> }.into_any(),
-        SectionLevel::H2 => view! { <H2>{title.clone()}</H2> }.into_any(),
-        SectionLevel::H3 => view! { <H3>{title.clone()}</H3> }.into_any(),
-        SectionLevel::H4 => view! { <H4>{title.clone()}</H4> }.into_any(),
+        SectionLevel::H1 => view! { <H1 align={align}>{title.clone()}</H1> }.into_any(),
+        SectionLevel::H2 => view! { <H2 align={align}>{title.clone()}</H2> }.into_any(),
+        SectionLevel::H3 => view! { <H3 align={align}>{title.clone()}</H3> }.into_any(),
+        SectionLevel::H4 => view! { <H4 align={align}>{title.clone()}</H4> }.into_any(),
     };
 
     view! {
-        <div class=format!("mt-{} mt-{}", space_above, space_above)>
+        <Stack size={if spaced { Size::Lg } else { Size::Sm }} add_class={class_str}>
             {heading}
             <Stack>
                 {children()}
             </Stack>
-        </div>
+        </Stack>
     }
 }
